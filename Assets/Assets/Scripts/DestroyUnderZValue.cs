b41e246f1c14d303ecbfde43746c846b25524e51
Sean Cole <msean.cole@gmail.com>
2014-08-13 00:13:07 -0400

    Pillar obstacles to dodge

---
+++ Assets/Assets/Scripts/DestroyUnderZValue.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyUnderZValue : MonoBehaviour {
+	public float destroyThreshold = 0.0f;
+
+	// Update is called once per frame
+	void Update () {
+		if( transform.position.z <= destroyThreshold ) {
+			Destroy( gameObject );
+		}
+	}
+}
---
+++ Assets/Assets/Scripts/RandomlySpawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomlySpawn : MonoBehaviour {
+	public GameObject entity;
+	public float minX = -10.0f;
+	public float maxX = 10.0f;
+	public float minY = -10.0f;
+	public float maxY = 10.0f;
+
+	public float minSpawnTime = 1.0f;
+	public float maxSpawnTime = 1.0f;
+
+	// Use this for initialization
+	void Start() {
+		Invoke( "SpawnNow", Random.Range( minSpawnTime, maxSpawnTime ) );
+	}
+	
+	void SpawnNow() {
+		Instantiate( entity, transform.position + new Vector3( Random.Range( minX, maxX ), Random.Range( minY, maxY ) ), Quaternion.identity );
+		Invoke( "SpawnNow", Random.Range ( minSpawnTime, maxSpawnTime ) );
+	}
+}
