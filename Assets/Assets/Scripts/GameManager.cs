ce6371f4a2bf1eb55234301f247fc64679776dba
Sean Cole <msean.cole@gmail.com>
2014-08-16 20:17:37 -0400

    EP6: Implemented score/highscore and unified speed system

--- Assets/Assets/Scripts/GameManager.cs
+++ Assets/Assets/Scripts/GameManager.cs
@@ -2,11 +2,17 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
+	public static GameManager instance;
+	public float pointsPerUnitTravelled = 1.0f;
+	public float gameSpeed = 10.0f;
+
+	private float score = 0.0f;
+	private static float highScore = 0.0f;
 	private bool gameOver = false;
 
 	// Use this for initialization
 	void Start () {
-	
+		instance = this;
 	}
 	
 	// Update is called once per frame
@@ -18,9 +24,20 @@ public class GameManager : MonoBehaviour {
 		if( gameOver && Input.anyKeyDown ) {
 			Application.LoadLevel( Application.loadedLevel );
 		}
+
+		if( ! gameOver ) {
+			score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+			if( score > highScore ) {
+				highScore = score;
+			}
+		}
 	}
 
 	void OnGUI() {
+		string currentScore = ( ( int ) score ).ToString();
+		string currentHighScore = ( ( int ) highScore ).ToString();
+		GUILayout.Label( "Score: " + currentScore );
+		GUILayout.Label ( "Highscore: " + currentHighScore );
 		if( gameOver == true ) {
 			GUILayout.Label ( "Game Over! Press any key to reset!" );
 		}
---
+++ Assets/Assets/Scripts/MoveBasedOnGameSpeed.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBasedOnGameSpeed : MonoBehaviour {
+	public Vector3 direction = Vector3.forward;
+
+	// Use this for initialization
+	void Start() {
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		transform.position += transform.rotation * direction.normalized * GameManager.instance.gameSpeed * Time.deltaTime;
+	}
+}
