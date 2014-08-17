a3899a3abe45af0eb181f0df0061a95a09f36ed6
Sean Cole <msean.cole@gmail.com>
2014-08-16 20:46:18 -0400

    EP7: Added title screen and a way to proceed to first level. Added scenes to build

---
+++ Assets/Assets/Scripts/LoadLevelOnAnyKey.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoadLevelOnAnyKey : MonoBehaviour {
+	public string levelName;
+
+	void Update () {
+		if( Input.anyKeyDown ) {
+			Application.LoadLevel( levelName );
+		}
+	}
+}
